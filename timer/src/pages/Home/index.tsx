5927867948f5ce691ba75d3594febb18080cf52b
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-04-23 23:38:16 -0300

    Conflict

--- timer/src/pages/Home/index.tsx
+++ timer/src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
-
+import { useState } from 'react'
 import {
   CountdownContainer,
   FormContainer,
@@ -20,18 +20,15 @@ const newCycleFormValidationSchema = zod.object({
     .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,
-    },
   })
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
   const task = watch('task')
   const isSubmitDisable = !task
